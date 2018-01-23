0beeac8843fc3d197d98a024d74b3ac7c28a7559
vidhya2499 <35724861+vidhya2499@users.noreply.github.com>
2018-01-23 15:01:51 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,6 @@
+a=int(input("enter number to check positive or negative"))
+if a>0:
+    print(a,"is postive")
+else:
+    print(a,"is negative")
+    
---
+++ program4.py
@@ -0,0 +1,5 @@
+a=int(input("enter number to check positive or negative"))
+if a>0:
+    print(a,"is positive")
+if a<0:
+    print(a,"is negative")
