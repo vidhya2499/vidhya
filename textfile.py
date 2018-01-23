3326ae72849a816b0c4cc848939650d3cacd057c
vidhya2499 <35724861+vidhya2499@users.noreply.github.com>
2018-01-23 15:56:59 +0530

    Add files via upload

---
+++ textfile.py
@@ -0,0 +1,5 @@
+descrption of python lab
+  we have enjoyed a lot by learning if, if else,else if,loop statements in the class
+  and we had played a snake and ladder program which made us to think something in the lab
+  and by this it was useful for us 
+  
