989e756dbfbd8b6161aa4aa4fd9a8d9cb3145c83
vidhya2499 <35724861+vidhya2499@users.noreply.github.com>
2018-02-20 14:54:35 +0530

    Add files via upload

--- program21.py
+++ program21.py
@@ -1,7 +1,10 @@
 import smtplib
 server=smtplib.SMTP('smtp.gmail.com', 587)
+l=["swathik.8685@gmail.com", "vidhyabidrakote2@gmail.com"]
+for i in l:
 server.starttls()
-server.login("vidhyabidrakote2@gmail.com","nagaraju9")
-msg = "hi!"
-server.sendmail("vidhyabidrakote2@gmail.com","swathi1213@gmail.com", hi)
+server.login("vidhyabidrakote2@gmail.com", "chiru243")
+msg ="hi!"
+server.sendmail("vidhyabidrakote2@gmail.com", l[i], msg)
+print("success")
 server.quit()
---
+++ program26.py
@@ -0,0 +1,8 @@
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("vidhyabidrakote2@gmail.com", "chiru243")
+msg = "hi!"
+server.sendmail("vidhyabidrakote2@gmail.com", "swathik.8586@gmail.com", msg)
+print("success")
+server.quit()
